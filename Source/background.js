04d031b6a618d045e20119e1fcf0e194168b18ff
Vlad Zamskoi <v.zamskoi@gmail.com>
2015-05-24 20:46:05 +0300

    Project files reorganization

--- Source/background.js
+++ Source/background.js
@@ -8,7 +8,7 @@ var selectedText;
 chrome.browserAction.onClicked.addListener(function () {
     // No tabs or host permissions needed!  
     chrome.tabs.executeScript(null, {
-        file: "content_script.js"
+        file: "Source/content_script.js"
     });
 });
 
