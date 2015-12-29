f8dd9c83affb2161aad3a0d0b9aab773a4cbf472
Vlad Zamskoi <v.zamskoi@gmail.com>
2015-12-29 11:45:27 +0300

    Seconds omitted from timestamp. Minor code cleaning.

--- Source/background.js
+++ Source/background.js
@@ -1,11 +1,11 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+chrome.runtime.onMessage.addListener(
+    function(request, sender) {
+        var textWithUrl = fromatTimeStampedTextAndUrl(request.selectedText, sender.tab.url);
+        copyToClipboard(textWithUrl);
+    }
+);
 
-// Called when the user clicks on the browser action.
-var selectedText;
-
-chrome.browserAction.onClicked.addListener(function () {
+chrome.browserAction.onClicked.addListener(function() {
     // No tabs or host permissions needed!  
     chrome.tabs.executeScript(null, {
         file: "Source/content_script.js"
@@ -13,15 +13,14 @@ chrome.browserAction.onClicked.addListener(function () {
 });
 
 chrome.commands.onCommand.addListener(function(command) {
-    if(command === "CopyWithURL"){
+    if (command === "CopyWithURL") {
         chrome.tabs.executeScript(null, {
             file: "Source/content_script.js"
         });
     }
 });
 
-//todo change function name
-function combineTextWithSourceURL(text, sourceURL) {
+function fromatTimeStampedTextAndUrl(text, sourceURL) {
     return text + " [" + timeStamp() + ", source: (" + sourceURL + ")]";
 }
 
@@ -51,41 +50,28 @@ function copyToClipboard(text) {
 }
 
 /**
- * Return a timestamp with the format "m/d/yy h:MM:ss TT"
+ * Return a timestamp with the format "m/d/yy h:MM TT"
  * @type {Date}
  */
 function timeStamp() {
-// Create a date object with the current time
-  var now = new Date();
+    // Create a date object with the current time
+    var now = new Date();
+    var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+    var time = [now.getHours(), now.getMinutes()];
 
-// Create an array with the current month, day and time
-  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+    var suffix = (time[0] < 12) ? "AM" : "PM";
 
-// Create an array with the current hour, minute and second
-  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+    // Convert hour from military time
+    time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
 
-// Determine AM or PM suffix based on the hour
-  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+    // If hour is 0, set it to '00'
+    time[0] = time[0] || '00';
 
-// Convert hour from military time
-  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
-
-// If hour is 0, set it to 12
-  time[0] = time[0] || 12;
-
-// If seconds and minutes are less than 10, add a zero
-  for ( var i = 1; i < 3; i++ ) {
-    if ( time[i] < 10 ) {
-      time[i] = "0" + time[i];
+    // If minutes are less than 10, add a zero
+    if (time[1] < 10) {
+        time[1] = "0" + time[1];
     }
-  }
 
-  return date.join("/") + " " + time.join(":") + " " + suffix;
+    return date.join("/") + " " + time.join(":") + " " + suffix;
 }
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender) {
-        var textWithUrl = combineTextWithSourceURL(request.selectedText, sender.tab.url);
-        copyToClipboard(textWithUrl);
-    }
-);
